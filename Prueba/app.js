929f767817c0669d5e896a5043d4a46c203227dc
rickyveeld <rickyvelediaz@gmail.com>
2025-09-09 19:43:49 -0600

    video de la 2

---
+++ Prueba/app.js
@@ -0,0 +1,38 @@
+
+/*
+-se actualiza constantemente por lo que siempre se debe estar aprendiendo
+-fue creado como una solucion universal para la web
+-permite que los sitios web responsivos
+-Scrpt enter en el index para ligar a la app de js y link para html
+*/
+"Hola mundo" //STRING
+'Hola mundo' //STRING no hay diferencia entre uno y otro
+123//number
+true  //boolean
+['ricky','perla','cruz'] //array (lista) empiezan desde 0
+{
+    nombre: 'richi';
+    edad: '19';
+}//object
+
+var ejemplo = "ejemplo"; //se declaran para poder usarse en todo el codigo
+let nombre = "Richi"; //let sirve para ciertos bloques de codigo como en un for
+const edad = "19"; //constantes
+//hacer uso de aboutblank.com
+document.write("<h1> Hola mundo</h1>");//para escribir en el documento
+//console.log(); para escribir en consola 
+/*
+operadores 
+"+,-,*,<,>"
+*/
+let clave = "juancho123";
+let result = input == clave; //estrictamente igual y input para entrada de datos
+/*
+condicionales
+if(result == true){
+accion a ejecutar
+}
+else{
+    accion a ejecutar si no se cumple la condicion
+    }
+*/
