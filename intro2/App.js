0bad3aa7b39da3f4e26b76a98bb6efa586720dd1
rickyveeld <rickyvelediaz@gmail.com>
2025-10-10 20:17:15 -0600

    practica 5
    creacion de botones de incremento, decremento y reinicio

--- intro2/App.js
+++ intro2/App.js
@@ -1,15 +1,21 @@
+//Zona de importaciones (zona 1)
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View,Button } from 'react-native';
+import React, {useState} from 'react';
+//Main Zona de componentes (zona 2)
 export default function App() {
+  const[contador,setContador]=useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Contador:{contador}</Text>
+      <Button title='Incrementar' onPress={()=>setContador(contador+1)}></Button>
+      <Button title='decremento' onPress={()=>setContador(contador-1)}></Button>
+      <Button title='reinicio' onPress={()=>setContador(contador - contador)}></Button>
       <StatusBar style="auto" />
     </View>
   );
 }
-
+//zona de estilos y posicionamiento()
 const styles = StyleSheet.create({
   container: {
     flex: 1,
