283d3a38714d4aac2f81e0bb94817aa65137e8a6
rickyveeld <rickyvelediaz@gmail.com>
2025-11-07 16:37:25 -0600

    practica botones
    (nosotros expusimos entonces no la tenia en el repositorio)

--- intro2/screens/BotonesScreens.js
+++ intro2/screens/BotonesScreens.js
@@ -1,32 +1,54 @@
-import { Text, StyleSheet, View, Buttonw } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import { View, Text, Button, Switch, StyleSheet } from 'react-native';
 
-export default function BotonesScreens () {
+export default function Botones2Screen() {
+  const [isOn, setIsOn] = useState(false);
+  const [color, setColor] = useState('#ffff33');
 
-    return (
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Control de luz</Text>
 
- 
-      <View>
-        <Text>Proximamente </Text>
-        <Button
-        title = 'amarillo'
-        onPress={()=>cambiarColor('yellow')}
-        ></Button>
-       <Button
-        title = 'azul'
-        onPress={()=>cambiarColor('blue')}
-        ></Button>
-        <Button
-        title = 'rojo'
-        onPress={()=>cambiarColor('red')}
-        ></Button>
+      <Text style={[styles.light, { color: isOn ? color : '#555' }]}>
+        {isOn ? 'Luz encendida' : 'Luz apagada'}
+      </Text>
 
-
-
-
-
+      <Switch
+        value={isOn}
+        onValueChange={() => setIsOn(!isOn)}
+        //thumbColor={{ true: '#f5dd4b', false: '#6c2f2fff' }}
+        trackColor={{ true: '#f5dd4b', false: '#888' }}
+      />
+      <View style={styles.buttonRow}>
+        <Button title="Amarilla" color="#f5e642" onPress={() =>setColor('#f5e642')} />
+        <Button title="Azul" color="#4a90e2" onPress={() =>setColor('#4a90e2')} />
+        <Button title="Roja" color="#e94f37" onPress={() =>setColor('#e94f37')} />
       </View>
-    )
+
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000000ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 40,
+    color: '#fff',
+    marginBottom: 20,
+    fontWeight: 'bold',
+  },
+  light: {
+    fontSize: 28,
+    marginBottom: 15,
+  },
+  buttonRow: {
+    flexDirection: 'row',
+    gap: 10,
+    marginTop: 15,
+  },
+});
