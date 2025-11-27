53ebca1a3b3dce08a5db37769a18bb44124b3695
rickyveeld <rickyvelediaz@gmail.com>
2025-11-26 21:46:47 -0600

    practica stacknavigation

---
+++ StackNavigation/screens/Home.js
@@ -0,0 +1,54 @@
+import { View, Text, Pressable, StyleSheet } from 'react-native'
+
+export default function Home({ navigation }) {
+    return (
+        <View style = {styles.container}>
+            <Text style={styles.title}>Bienvenido a la pantalla principal</Text>
+            
+            <Pressable style={[styles.button, styles.buttonProfile]} onPress={() => navigation.navigate('Profile')}>
+                <Text style={styles.buttonText}>Ir a Perfil</Text>
+            </Pressable>
+
+            <Pressable style={[styles.button, styles.buttonSettings]} onPress={() => navigation.navigate('Settings')}>
+                <Text style={styles.buttonText}>Ir a Configuración</Text>
+            </Pressable>
+            
+            
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+    },
+    title: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        marginBottom: 40,
+        textAlign: 'center',
+    },
+    button: {
+        paddingVertical: 12,
+        paddingHorizontal: 30,
+        borderRadius: 8,
+        marginBottom: 20,
+        width: '80%',
+        alignItems: 'center',
+    },
+    buttonProfile: {
+        backgroundColor: '#e0b7b7ff',
+    },
+    buttonSettings: {
+        backgroundColor: '#19989cff',
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: '600',
+    }
+})
