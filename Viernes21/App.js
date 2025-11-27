ddb2b01fbbf68cdf259b91b022a9d4f79beb8610
rickyveeld <rickyvelediaz@gmail.com>
2025-11-27 16:04:22 -0600

    practica iconos terminada

--- Viernes21/App.js
+++ Viernes21/App.js
@@ -1,12 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
+import Home from "./screens/home";
+import Profile from "./screens/profile";
+import Settings from "./screens/settings";
+import { View, Text, StyleSheet } from "react-native";
+const Tab = createBottomTabNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+   <NavigationContainer>
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+          if (route.name === 'Home') {
+            iconName = 'home-outline';
+          } else if (route.name === 'Profile') {
+            iconName = 'person-outline';
+          } else if (route.name === 'Settings') {
+            iconName = 'settings-outline';
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#007BFF',
+        tabBarInactiveTintColor: 'gray',
+        tabBarStyle: {
+          paddingBottom: 5,
+          height: 60,
+        },
+      })}
+    >
+
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen name="Settings" component={Settings} />
+    </Tab.Navigator>
+   </NavigationContainer>
+
   );
 }
 
